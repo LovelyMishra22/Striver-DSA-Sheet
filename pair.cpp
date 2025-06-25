398a93214c24e70a4510dda87fdb8706e7eb0e6d
LovelyMishra22 <lovely.mishra.tech@gmail.com>
2025-06-25 18:22:13 +0530

    Added pair.cpp for STL basics.

---
+++ pair.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+void explainPair()
+{
+    pair<int,int> p={1,2};
+    cout<<p.first<<" "<<p.second<<endl;
+    pair<int,pair<int,int>> q={1,{2,3}};
+    cout<<q.first<<" "<<q.second.first<<" "<<q.second.second<<endl;
+    pair<int,int> arr[]={{1,2},{3,4},{5,6}};
+    cout<<arr[1].second<<endl;
+}
+int main()
+{
+    explainPair();
+    return 0;
+}
