f7ee4ba3dccf28be196e2fd9e378bb9a45d36ca5
LovelyMishra22 <lovely.mishra.tech@gmail.com>
2025-07-10 16:31:36 +0530

    Add problem description to sum.cpp

--- arrays/missing_number/sum.cpp
+++ arrays/missing_number/sum.cpp
@@ -1,3 +1,15 @@
+/*
+    Given an integer array of size n containing distinct values in the range from 0 to n (inclusive), return the only number missing from the array within this range.
+    
+    Input: nums = [0, 2, 3, 1, 4]
+    Output: 5
+
+    Input: nums = [0, 1, 2, 4, 5, 6]
+    Output: 3
+
+    TC: O(N) 
+    SC: O(1)
+*/
 #include <bits/stdc++.h>
 using namespace std;
 int missingNumber(vector<int> &nums)
